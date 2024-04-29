461dc6c635cdcdb1ba5a92b18cfa0d05ff8975ee
Travis Silva <60702045+TheRealGeoDash2019@users.noreply.github.com>
2024-04-29 13:19:29 -0400

    Update PluginManager.js

--- plugins/PluginManager.js
+++ plugins/PluginManager.js
@@ -4,19 +4,26 @@ install({
   icon: "extension" // Icon from https://fonts.google.com/icons
 }, async function(event, end) {
   const pluginUrl = prompt(`Paste a valid link to a plugin`);
-  const pluginMeta = await rubidiumPlugins.getPluginMetadata(pluginUrl);
-  if (pluginMeta) {
-    console.log(`[Rubidium Plugins] Install request:`, pluginMeta);
-    const confirmation = confirm(`You are about to install:\n${pluginMeta.details.name}\nby ${pluginMeta.details.author ?? "Unknown Author"}`);
-    if (confirmation === true) {
-      console.log(`[Rubidium Plugins] Installing...`);
-      rubidiumPlugins.requestPluginInstall(pluginUrl);
+  try {
+    const { hostname, pathname } = new URL(pluginUrl, location.href);
+    const proxyLocation = (location.protocol + "//" + location.hostname + "/proxy/" + hostname + pathname);
+    const pluginMeta = await rubidiumPlugins.getPluginMetadata(proxyLocation);
+    if (pluginMeta) {
+      console.log(`[Rubidium Plugins] Install request:`, pluginMeta);
+      const confirmation = confirm(`You are about to install:\n${pluginMeta.details.name}\nby ${pluginMeta.details.author ?? "Unknown Author"}`);
+      if (confirmation === true) {
+        console.log(`[Rubidium Plugins] Installing...`);
+        rubidiumPlugins.requestPluginInstall(proxyLocation);
+      } else {
+        console.log(`[Rubidium Plugins] Installed cancelled.`);
+      }
     } else {
-      console.log(`[Rubidium Plugins] Installed cancelled.`);
-    }
-  } else {
-    alert(`Invalid plugin link`)
+      alert(`Invalid plugin link`)
+      console.log(`[Rubidium Plugins] Invalid plugin link.`);
+    };
+  } catch {
+    alert("Invalid plugin link")
     console.log(`[Rubidium Plugins] Invalid plugin link.`);
-  };
+  }
   end();
 });
