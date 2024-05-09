77e815d55f6dafa95917ac7778078bb290999401
Travis Silva <60702045+TheRealGeoDash2019@users.noreply.github.com>
2024-05-09 19:35:19 -0400

    Update TextToConversation_2.js

--- plugins/TextToConversation_2.js
+++ plugins/TextToConversation_2.js
@@ -3,11 +3,13 @@ install({
   icon: "text_snippet" // Icon from https://fonts.google.com/icons
 }, async function(event, end) {
   // Number.prototype.fileSize()
-  Object.defineProperty(Number.prototype,'fileSize',{value:function(a,b,c,d){
-   return (a=a?[1e3,'k','B']:[1024,'K','iB'],b=Math,c=b.log,
-   d=c(this)/c(a[0])|0,this/b.pow(a[0],d)).toFixed(2)
-   +' '+(d?(a[1]+'MGTPEZY')[--d]+a[2]:'Bytes');
-  },writable:false,enumerable:false});
+  if (!("fileSize" in Number.prototype)) {
+    Object.defineProperty(Number.prototype,'fileSize',{value:function(a,b,c,d){
+     return (a=a?[1e3,'k','B']:[1024,'K','iB'],b=Math,c=b.log,
+     d=c(this)/c(a[0])|0,this/b.pow(a[0],d)).toFixed(2)
+     +' '+(d?(a[1]+'MGTPEZY')[--d]+a[2]:'Bytes');
+    },writable:false,enumerable:false});
+  }
   // Get Text Files
   const getTextFile = async function() {
     const fileHandles = await window.showOpenFilePicker({ multiple: false, startIn: "documents", excludeAcceptAllOption: true, types: [{
