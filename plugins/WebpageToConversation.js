95c74c2f6aaf63da30177b32c585e67cd4fb74b4
Travis Silva <60702045+TheRealGeoDash2019@users.noreply.github.com>
2024-05-17 11:35:24 -0400

    Update WebpageToConversation.js

--- plugins/WebpageToConversation.js
+++ plugins/WebpageToConversation.js
@@ -22,7 +22,7 @@ install({
         const dom = new DOMParser().parseFromString(contents, "text/html");
         Array.from(dom.querySelectorAll("script")||[]).forEach(s => s.remove());
         Array.from(dom.querySelectorAll("style")||[]).forEach(s => s.remove());
-        return (dom.body.innerText || dom.body.textContent).trim().replace(/\s{3,}/gmi, " ").replace(/\n{3,}/gm, "\n").trim();
+        return (dom.body.innerText || dom.body.textContent).trim().replace(/\s{3,}/gmi, " ").replace(/\n{3,}/gm, " ").trim();
       }
     } catch (err) {
       return "No content available. Reason: Invalid Webpage";
