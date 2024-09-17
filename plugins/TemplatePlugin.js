874a5b071f21899ce307de33eec27c1106656316
Travis Silva <60702045+TheRealGeoDash2019@users.noreply.github.com>
2024-09-17 12:45:26 -0400

    Update TemplatePlugin.js

--- plugins/TemplatePlugin.js
+++ plugins/TemplatePlugin.js
@@ -1,5 +1,6 @@
 install({
   name: "Template Plugin", // Plugin Name
+  description: "Template Plugin useful for Developers",
   icon: "add" // Icon from https://fonts.google.com/icons
 }, function(event, end) {
   console.log(event);
